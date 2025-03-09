05ad23cdd970feefbfd2e99f8bc2c6384be610f9
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-03-09 18:59:17 +0800

    theme added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
     <div>
-      <h1>This is HomePage component</h1>
+      <Button>Click Me</Button>
     </div>
   );
 };
