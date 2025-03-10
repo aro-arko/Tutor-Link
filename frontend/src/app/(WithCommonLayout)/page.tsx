34a42c5e80f86e3163f8a1114b777d590df89309
Abidur Rahman Arko <aroarko.sd@gmail.com>
2025-03-10 14:44:37 +0800

    features added

--- frontend/src/app/(WithCommonLayout)/page.tsx
+++ frontend/src/app/(WithCommonLayout)/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Banner from "@/components/modules/home/banner/Banner";
+import Features from "@/components/modules/home/features/Features";
 import Sponsors from "@/components/modules/home/sponsors/Sponsors";
 import { useUser } from "@/context/UserContext";
 
@@ -10,6 +11,7 @@ const HomePage = () => {
     <div>
       <Banner />
       <Sponsors />
+      <Features />
     </div>
   );
 };
---
+++ frontend/src/components/modules/home/features/Features.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { ShieldCheck, Search, CreditCard } from "lucide-react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
+const features = [
+  {
+    icon: <Search className="h-12 w-12 text-red-600" />,
+    title: "Find Tutors Fast",
+    description: "Search and book in seconds.",
+  },
+  {
+    icon: <CreditCard className="h-12 w-12 text-red-600" />,
+    title: "Secure Payments",
+    description: "Your transactions are 100% safe.",
+  },
+  {
+    icon: <ShieldCheck className="h-12 w-12 text-red-600" />,
+    title: "Verified Profiles",
+    description: "All tutors go through background checks.",
+  },
+];
+
+const Features = () => {
+  return (
+    <section className="pt-12">
+      <div className="py-8">
+        <div className="max-w-7xl mx-auto px-6 text-center">
+          {/* Section Title */}
+          <h2 className="text-4xl font-extrabold text-gray-900 mb-12">
+            Why Choose Us?
+          </h2>
+          {/* Features Grid */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+            {features.map((feature, index) => (
+              <Card
+                key={index}
+                className="flex flex-col items-center justify-center text-center p-8 h-[280px] bg-white shadow-md  rounded-xl"
+              >
+                <CardHeader className="flex flex-col items-center">
+                  <div className="flex justify-center mb-4">{feature.icon}</div>
+                  <CardTitle className="text-2xl font-semibold text-gray-900">
+                    {feature.title}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <CardDescription className="text-gray-600 text-base">
+                    {feature.description}
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
